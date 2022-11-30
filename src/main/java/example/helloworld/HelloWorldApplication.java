2c6abfc15e341484b49cfe7ad123ab3d6733bd3b
Madhura Bhave <bhavem@vmware.com>
2022-11-30 15:53:16 -0800

    Initial commit

---
+++ src/main/java/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
